1a292cd4acf07eaace25a56f3345f516eee07296
Damien Irving <irving.damien@gmail.com>
2025-11-06 12:25:20 +1100

    Fix bugs in BARRA code

--- preprocess-barra.sh
+++ preprocess-barra.sh
@@ -31,9 +31,9 @@ if [[ "${input_var}" == "wbgt" ]] ; then
 else
     indir=/g/data/ob53/BARRA2/output/reanalysis/AUS-11/BOM/ERA5/historical/hres/BARRA-R2/v1/${input_freq}/${input_var}/latest
 fi
-outdir=/g/data/ia39/australian-climate-service/test-data/BARRA2/output/reanalysis/${output_grid}/BOM/ERA5/historical/hres/BARRAR2/v1/${output_freq}/${output_var}/latest-${chunking}
+outdir=/g/data/ia39/australian-climate-service/test-data/BARRA2/output/reanalysis/${output_grid}/BOM/ERA5/historical/hres/BARRAR2/v1/${output_freq}/${output_var}/latest-${regrid_method}-${chunking}
 for year in $(seq 1979 2024); do
-    infiles=(`ls ${indir}/${input_var}_*_${year}??-${year}??.nc`)
+    infiles=(`ls ${indir}/${input_var}_*_ERA5_historical_hres_BOM_BARRA-R2_v1_${input_freq}_${year}??-${year}??*`)
     outfile=${output_var}_${output_grid}_ERA5_historical_hres_BOM_BARRAR2_v1_${output_freq}_${year}0101-${year}1231.nc
     
     python_command="${python} preprocess.py ${infiles[@]} ${input_var} ${input_freq} ${output_var} ${output_freq} ${output_grid} ${regrid_method} ${outdir}/${outfile} --chunking_strategy ${chunking} --min_chunk_size ${chunksize} --compute"
